b0831c64a876e50ea81c4c87b67b5adc68a2a049
Pavel Grigorenko <311639@niuitmo.ru>
2022-01-21 16:41:02 +0300

    Threshold

--- params.h
+++ params.h
@@ -10,8 +10,8 @@ constexpr QPointF DEFAULT_CENTER{-0.5, 0};
 constexpr unsigned MINIMAL_MAX_ITERATIONS = 0;
 constexpr unsigned DEFAULT_MAX_ITERATIONS = 200;
 constexpr unsigned MAXIMAL_MAX_ITERATIONS = 2000;
-constexpr qreal MINIMAL_THRESHOLD = 1 << 1;
-constexpr qreal DEFAULT_THRESHOLD = 1 << 15;
+constexpr qreal MINIMAL_THRESHOLD = 4;
+constexpr qreal DEFAULT_THRESHOLD = 4;
 constexpr qreal MAXIMAL_THRESHOLD = 1 << 30;
 constexpr unsigned CHECK_CANCEL_EVERY_ITERATIONS = 1 << 5;
 
